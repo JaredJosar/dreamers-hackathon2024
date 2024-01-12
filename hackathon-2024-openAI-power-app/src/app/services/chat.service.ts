beec5382dfbd5e78aebfde2ec0c35a65b3cb202e
Ben Miller <ben.miller@gomomentus.com>
2024-01-12 11:06:39 -0600

    updated the prompts to describe events and functions

--- hackathon-2024-openAI-power-app/src/app/services/chat.service.ts
+++ hackathon-2024-openAI-power-app/src/app/services/chat.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { OpenAI } from 'openai';
+import { PromptService } from './prompt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  private apiUrl = 'https://api.openai.com/v1/chat/completions'; 
+  private apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private promptService: PromptService) { }
 
   async sendMessage(userMessage: string, previousMessages: string[]): Promise<Observable<any>> {
     
@@ -17,7 +18,7 @@ export class ChatService {
       let messagesRequestData: any[] = this.getMessageRequestData(userMessage, previousMessages);
 
       // Initialize OpenAI
-      const openai = new OpenAI({ apiKey: "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", dangerouslyAllowBrowser: true });
+      const openai = new OpenAI({ apiKey: "Replace with actual API Key.  Cannot put it on Github.", dangerouslyAllowBrowser: true });
 
       // Send the message to OpenAI and await a response
       const chatCompletion = await openai.chat.completions.create({
@@ -41,7 +42,7 @@ export class ChatService {
   }
 
   getMessageRequestData(userMessage: string, previousMessages: string[]): any[] {
-    let initializationPrompt: string = this.getInitializationPrompt();
+    let initializationPrompt: string = this.promptService.getInitializationPrompt();
 
     let messagesRequestData: any[] =
       [
@@ -53,28 +54,4 @@ export class ChatService {
     return messagesRequestData;
   }
 
-  getInitializationPrompt() {
-    return this.getIntroPrompt() + this.getEventPrompt() + this.getFunctionPrompt() + this.getOutputPrompt();
-  }
-
-  getIntroPrompt() {
-    return "The user will describe events and functions.   You will output your responses in JSON format that can be sent to another API.  " +
-           "An event represents a real - world event at a venue.  A function is a session within that event.Events can have multiple functions.";
-  }
-
-  getEventPrompt() {
-    return "An event's JSON will have the following keys: ID, EventDescription, EventStartDate, EventStartTime, EventEndDate, EventEndTime." + 
-            "ID will be the primary key.For new events, the ID will be - 1.  " +
-            "EventDescription describes the event.EventStartDate is the date the event starts.EventStartTime is the time of day the event starts." +
-            "EventEndDate is the date the event ends.EventEndTime is the time of day the event ends.";
-  }
-
-  getFunctionPrompt() {
-    return "Functions are similar to events, but like they are like smaller events within the event.  " +
-           "They will have the keys ID, FunctionDescription, FunctionStartDate, FunctionStartTime, FunctionEndDate, FunctionEndTime.";
-  }
-
-  getOutputPrompt() {
-    return "The user will describe an event and you will output JSON data that represents the event and functions to create.";
-  }
 }
---
+++ hackathon-2024-openAI-power-app/src/app/services/prompt.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PromptService {
+  public getInitializationPrompt() {
+    return this.getIntroPrompt() + this.getEventPrompt() + this.getFunctionPrompt() + this.getOutputPrompt();
+  }
+
+  private getIntroPrompt() {
+    return "The user will describe events and functions.  You will help them create JSON data to represent an Event or Function once you have enough information.  ";
+  }
+
+  private getEventPrompt() {
+    return "An event represents a real-world event at a venue.  " + 
+      "An event's JSON will have the following keys: ID, Description, Organization, Account, StartDate, EndDate, Status.  " +
+      "ID will be the primary key.  For new events, the ID will start at - 1 and decrement by 1 for each new event.  Example -1, -2, -3, etc.  " +
+      "Organization is a string with two characters.  If the user doesn't mention an Org or Organization or Org Code with two characters, default the value to D2.  " +
+      "Description is a string that describes the event.  StartDate is a DateTime that represents the date the event starts." +
+      "EndDate is a DateTime that represents the date the event ends.  " +
+      "Account is a string with 8 characters that represents the company or person holding the event.  If no Account or AccountCode is mentioned, default the value to OPENAI.  " +
+      "Status is a two character string that will be set to AI.";
+  }
+
+  private getFunctionPrompt() {
+    return "Events can have multiple functions.  Functions are similar to events, but like they are like smaller events within the event.  " +
+      "They will have the keys ID, Description, EventID, StartDate, EndDate, StatusCode." +
+      "EventID will be the ID from the Event.  Description is a string that describes the function.  StartDate is a datetime that represents the date the function starts.  " +
+      "EndDate is a datetime that represents the date the function ends." +
+      "StatusCode is that status of the function.  It is a string with two characters.  Default its value to AI if a two-character status is not mentioned.  ";
+  }
+
+  private getOutputPrompt() {
+    return "The user will describe an event and you will output JSON data that represents the event and functions to create." +
+           "If you do not have enough information to create JSON data for an event or function, chat with the user as an assistant instead of outputting JSON.";
+  }
+}
